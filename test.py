f4b9fff9fda683830d942c5914fb1146524cab16
Tobias Buess <tobias.buess2001@gmail.com>
2024-11-23 11:50:14 +0100

    better functions

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ import imageio.v3 as imageio
 import numpy as np
 import glob
 
-
 class time_region:
     def __init__(self, time_offset=0):
         self._time_offset = time_offset
@@ -14,13 +13,12 @@ class time_region:
     def __enter__(self):
         self._t_start = time.time()
         return self
-
+    
     def __exit__(self, exec_type, exec_value, traceback):
         self._t_end = time.time()
 
     def elapsed_time(self):
         return self._time_offset + (self._t_end - self._t_start)
-
 
 class time_region_cuda:
     def __init__(self, time_offset=0, cuda_stream=0):
@@ -28,7 +26,7 @@ class time_region_cuda:
         self._t_end = cuda.event(timing=True)
         self._time_offset = time_offset
         self._cuda_stream = cuda_stream
-
+    
     def __enter__(self):
         self._t_start.record(self._cuda_stream)
         return self
@@ -38,10 +36,22 @@ class time_region_cuda:
         self._t_end.synchronize()
 
     def elapsed_time(self):
-        return self._time_offset + 1e-3 * cuda.event_elapsed_time(
-            self._t_start, self._t_end
-        )
+        return self._time_offset + 1e-3*cuda.event_elapsed_time(self._t_start, self._t_end)
 
+def reconstruct_svd_broadcast(u,s,vt,k):
+    """SVD reconstruction for k components using broadcast
+    
+    Inputs:
+    u: (m,n) numpy array
+    s: (n) numpy array (diagonal matrix)
+    vt: (n,n) numpy array
+    k: number of reconstructed singular components
+    
+    Ouput:
+    (m,n) numpy array U_mk * S_k * V^T_nk for k reconstructed components
+    """
+
+    return u[:, :k]@(s[:k].reshape(-1, 1)*vt[:k, :])
 
 @cuda.jit("void(Array(float32, 2, 'C'), Array(float32, 1, 'C'), Array(float32, 2, 'F'), int32, Array(float32, 2, 'C'))")
 def svd_reco_kernel(u, s, vt, k, y):
@@ -65,7 +75,16 @@ def svd_reco_kernel(u, s, vt, k, y):
 
     y[m, n] = element
 
-def calculate(
+def get_transfers_fpo_per_thread(k):
+    """ Standard function to estimate number of fp32 transfers and operations per thread
+    """
+    
+    num_transfers_per_thread = 1 + (4 * k) + 1
+    num_fpo_per_thread = k*2
+
+    return num_transfers_per_thread, num_fpo_per_thread
+
+def svd_reco_cuda(
     u: np.ndarray, s: np.ndarray, vt: np.ndarray, k: int, block_size: tuple = (32, 32)
 ) -> np.ndarray:
     """Host function to perform SVD reconstruction using CUDA kernel.
@@ -84,10 +103,57 @@ def calculate(
     # convert to correct dtype
     u = u.astype(np.float32)
     s = s.astype(np.float32)
-    vt = np.asfortranarray(vt.astype(np.float32))
+    vt = np.asfortranarray(vt.astype(np.float32)) # convert and make column major
+
+    # Send arrays to gpu
+    u = cuda.to_device(u)
+    s = cuda.to_device(s)
+    vt = cuda.to_device(vt)
+
+    # create array where results are stored. Also pin that array so no data gets moved out of the ram.
+    m, n = u.shape[0], vt.shape[1]
+    y = cuda.device_array((m, n), dtype=np.float32)
+    y_ret = cuda.pinned_array_like(y)
+
+    # Define CUDA thread and block dimensions
+    blocks_per_grid_x = (m + block_size[0] - 1) // block_size[0]
+    blocks_per_grid_y = (n + block_size[1] - 1) // block_size[1]
+    blocks_per_grid = (blocks_per_grid_x, blocks_per_grid_y)
+
+    # launch cuda kernel
+    svd_reco_kernel[blocks_per_grid, block_size](u, s, vt, k, y)
+
+    y.copy_to_host(y_ret)
+
+    return y_ret
+
+def svd_reco_cuda_metrics(u: np.ndarray, s: np.ndarray, vt: np.ndarray, k: int, block_size: tuple):
+    """Host function to perform SVD reconstruction using CUDA kernel and doing performance measurements.
+
+    Args:
+        u (np.ndarray): Left singular vectors, shape (m, n).
+        s (np.ndarray): Singular values, shape (n,).
+        vt (np.ndarray): Right singular vectors, shape (n, n).
+        k (int): Number of singular components to use in reconstruction.
+        block_size (tuple(int,int)): Number of threads per block
+
+    Returns:
+        dict containing times including memory bandwidth and TFLOPs
+    """
+
+    # convert to correct dtype
+    u = u.astype(np.float32)
+    s = s.astype(np.float32)
+    vt = vt.astype(np.float32)
+
+    # reconstruct using reference function
+    y_ref = reconstruct_svd_broadcast(u, s, vt, k)
+
+    # make column major
+    vt = np.asfortranarray(vt)
 
     with time_region_cuda() as t_xfer:
-        # Ensure inputs are in the correct dtype and order
+        # Send arrays to gpu
         u = cuda.to_device(u)
         s = cuda.to_device(s)
         vt = cuda.to_device(vt)
@@ -109,26 +175,25 @@ def calculate(
     with time_region_cuda(t_xfer.elapsed_time()) as t_xfer:
         # copy back to host
         y.copy_to_host(y_ret)
+    
+    # get number of transfers and number of fp32 operations per thread
+    num_transfers_per_thread, num_fpo_per_thread = get_transfers_fpo_per_thread(k)
 
-    # stop profiling
-    cuda.profile_stop()
-
-    # calculate number of transfers done in each thread
-    num_transfers_per_thread = 1 + (4 * k) + 1
-    number_of_GB_transferred = (
+    # calculate number of transfers in total
+    number_of_GB_transferred_total = (
         1e-9 * 4 * num_transfers_per_thread * y.shape[0] * y.shape[1]
     )
 
     # calculate number of floating point operations
-    num_fpo_per_thread = k*2
     num_fpo_total = 1e-12 * num_fpo_per_thread * y.shape[0] * y.shape[1]
-
-    print(f"Cuda transfer overhead: {t_xfer.elapsed_time()*1000} ms")
-    print(f"Cuda kernel time: {t_kernel.elapsed_time()*1000} ms")
-    print(f"Consumed memory bandwidth: {number_of_GB_transferred / t_kernel.elapsed_time()} GB/s")
-    print(f"Consumed TFLOPs: {num_fpo_total / t_kernel.elapsed_time()}")
-
-    return y_ret
+    
+    return {
+        "time_transfer_ms": t_xfer.elapsed_time()*1000,
+        "time_kernel_ms": t_kernel.elapsed_time()*1000,
+        "consumed_mem_bandwidth_GB/s": number_of_GB_transferred_total / t_kernel.elapsed_time(),
+        "consumed TFLOPs": num_fpo_total / t_kernel.elapsed_time(),
+        "isclose": np.isclose(y_ref, y_ret)
+    }
 
 if __name__ == "__main__":
     subfolder = "001"
@@ -152,4 +217,5 @@ if __name__ == "__main__":
     s = s.astype(np.float32)
     vt = vt.astype(np.float32)
 
-    calculate(u, s, vt, 100)
+    # do a full reconstruction
+    svd_reco_cuda_metrics(u, s, vt, 2000)
