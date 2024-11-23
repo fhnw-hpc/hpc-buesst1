8ed8f93fe95f8dae47b67072f5f78ca1ea171527
Tobias Buess <tobias.buess2001@gmail.com>
2024-11-23 16:39:32 +0100

    wrong size

--- test.py
+++ test.py
@@ -214,4 +214,4 @@ if __name__ == "__main__":
     u, s, vt = np.linalg.svd(im, full_matrices=False)
 
     # do a full reconstruction
-    svd_reco_cuda_metrics(u, s, vt, len(s), (256, 256))
+    svd_reco_cuda_metrics(u, s, vt, len(s), (32, 32))
