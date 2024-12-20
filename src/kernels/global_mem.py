a38386840161a3f66a1b023ffe94061453e56779
Tobias Buess <tobias.buess2001@gmail.com>
2024-12-20 14:36:54 +0100

    kernel signature fix

--- src/kernels/global_mem.py
+++ src/kernels/global_mem.py
@@ -2,7 +2,7 @@ from numba import cuda, float32, float64
 
 
 @cuda.jit(
-    "void(float64[:,::1], float64[::1], float64[:,::1], int32, float64[:,::1])",
+    "void(Array(float64, 2, 'C'), Array(float64, 1, 'C'), Array(float64, 2, 'C'), int32, Array(float64, 2, 'C'))",
     fastmath=True,
 )
 def fp64(u, s, vt, k, y):
@@ -20,7 +20,7 @@ def fp64(u, s, vt, k, y):
 
 
 @cuda.jit(
-    "void(float32[:,::1], float32[::1], float32[:,::1], int32, float32[:,::1])",
+    "void(Array(float32, 2, 'C'), Array(float32, 1, 'C'), Array(float32, 2, 'C'), int32, Array(float32, 2, 'C'))",
     fastmath=True,
 )
 def fp32(u, s, vt, k, y):
