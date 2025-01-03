8d0843e768051fa17cb492bfa71d4843ca09c077
Tobias Buess <tobias.buess2001@gmail.com>
2025-01-03 15:29:08 +0100

    better configuration

--- run_kernels.py
+++ run_kernels.py
@@ -15,9 +15,10 @@ from src.kernels.shared_mem import fp32 as kernel_sharedmem_fp32
 from src.kernels.shared_mem import fp64 as kernel_sharedmem_fp64
 from src.kernels.shared_mem import TILE_SIZE
 
-RECO_SHAPE = (3072, 4096)
+RECO_SHAPE = (768, 1024)
 BLOCK_SIZE = (8, 16)
 PIN_MEMORY = True
+NUM_STREAMS = 50
 
 # because of tiling -> num threads must be >= tile size because otherwise not all elements will be loaded
 assert (
@@ -30,22 +31,22 @@ if __name__ == "__main__":
 
     ref = reconstruct_svd_broadcast(*input)
 
-    reco_func = make_reconstructor(kernel_globalmem_fp64, BLOCK_SIZE)
+    reco_func = make_reconstructor(kernel_globalmem_fp64, BLOCK_SIZE, PIN_MEMORY)
     print("globalmem fp64 reco precise: ", compare_matrices(reco_func(*input), ref))
 
-    reco_func = make_reconstructor(kernel_globalmem_fp32, BLOCK_SIZE)
+    reco_func = make_reconstructor(kernel_globalmem_fp32, BLOCK_SIZE, PIN_MEMORY)
     print("globalmem fp32 reco precise: ", compare_matrices(reco_func(*input), ref))
 
-    reco_func = make_reconstructor(kernel_sharedmem_fp64, BLOCK_SIZE)
+    reco_func = make_reconstructor(kernel_sharedmem_fp64, BLOCK_SIZE, PIN_MEMORY)
     print("sharedmem fp64 reco precise: ", compare_matrices(reco_func(*input), ref))
 
-    reco_func = make_reconstructor(kernel_sharedmem_fp32, BLOCK_SIZE)
+    reco_func = make_reconstructor(kernel_sharedmem_fp32, BLOCK_SIZE, PIN_MEMORY)
     print("sharedmem fp32 reco precise: ", compare_matrices(reco_func(*input), ref))
 
     reco_func = make_reconstructor(
         kernel_globalmem_fp64,
         BLOCK_SIZE,
-        pin_memory=True,
+        PIN_MEMORY,
         use_streams=True,
     )
 
@@ -54,7 +55,7 @@ if __name__ == "__main__":
         [
             compare_matrices(result, ref)
             for result in reco_func(
-                *([input[0]] * 4, [input[1]] * 4, [input[2]] * 4, [input[3]] * 4)
+                *([input[0]] * NUM_STREAMS, [input[1]] * NUM_STREAMS, [input[2]] * NUM_STREAMS, [input[3]] * NUM_STREAMS)
             )
         ],
     )
